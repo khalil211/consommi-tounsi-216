c887b75467c159174ce19e9e1d6bd78773e9b369
khalil211 <khalilmajoul999@gmail.com>
2021-04-15 16:49:00 +0100

    User profile + addresses + admin

--- ConsommiTounsi/Controllers/UserController.cs
+++ ConsommiTounsi/Controllers/UserController.cs
@@ -1,24 +1,14 @@
 ﻿using ConsommiTounsi.Models.User;
-using System;
+using ConsommiTounsi.Utils;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ConsommiTounsi.Controllers
 {
     public class UserController : Controller
     {
-        private HttpClient httpClient;
-
-        public UserController()
-        {
-            httpClient = new HttpClient(new HttpClientHandler() { UseCookies = false });
-            httpClient.BaseAddress = new Uri("http://localhost:8080/api/");
-            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-        }
-
         public ActionResult Register()
         {
             return View();
@@ -27,23 +17,18 @@ namespace ConsommiTounsi.Controllers
         [HttpPost]
         public ActionResult Register(User user)
         {
+            HttpClient httpClient = HttpClientBuilder.get();
             if (ModelState.IsValid)
             {
                 HttpResponseMessage response = httpClient.PostAsJsonAsync<User>("users", user).Result;
                 response.EnsureSuccessStatusCode();
-                string message;
-                try
-                {
-                    message = response.Content.ReadAsStringAsync().Result;
-                }
-                catch (HttpRequestException e)
-                {
-                    message = e.Message;
-                }
-
+                string message = response.Content.ReadAsStringAsync().Result;
                 if (message.Equals("\"SUCCESS\""))
                     return Redirect("Login");
-                ViewBag.Message = message;
+                else if (message.Equals("\"USERNAME_ALREADY_EXISTS\""))
+                    ModelState.AddModelError("username", "Username already exists");
+                else if (message.Equals("\"EMAIL_ALREADY_EXISTS\""))
+                    ModelState.AddModelError("email", "Email already exists");
             }
             return View(user);
         }
@@ -56,6 +41,7 @@ namespace ConsommiTounsi.Controllers
         [HttpPost]
         public ActionResult Login(User user)
         {
+            HttpClient httpClient = HttpClientBuilder.get();
             httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "multipart/form-data");
             var loginForm = new MultipartFormDataContent
             {
@@ -82,10 +68,188 @@ namespace ConsommiTounsi.Controllers
 
         public ActionResult Logout()
         {
-            httpClient.DefaultRequestHeaders.Add("Cookie", (string)Session["api-cookie"]);
-            httpClient.GetAsync("lougout");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            httpClient.GetAsync("logout");
             Session.Clear();
             return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Details()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            return View(user);
+        }
+
+        public ActionResult Edit()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            User u = (User)Session["user"];
+            if (u == null)
+                return Redirect("Login");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            user.id = u.id;
+            HttpResponseMessage response = httpClient.PostAsJsonAsync<User>("customer/users/edit", user).Result;
+            response.EnsureSuccessStatusCode();
+            string message = response.Content.ReadAsStringAsync().Result;
+            if (message.Equals("\"SUCCESS\""))
+            {
+                Session["user"] = user;
+                return Redirect("Details");
+            }
+            else if (message.Equals("\"USERNAME_ALREADY_EXISTS\""))
+                ModelState.AddModelError("username", "Username already exists");
+            else if (message.Equals("\"EMAIL_ALREADY_EXISTS\""))
+                ModelState.AddModelError("email", "Email already exists");
+            return View(user);
+        }
+
+        public ActionResult Password()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Password(PasswordEdit password)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "customer/users/" + user.id + "/password";
+            HttpResponseMessage response = httpClient.PostAsJsonAsync<PasswordEdit>(url, password).Result;
+            response.EnsureSuccessStatusCode();
+            string message = response.Content.ReadAsStringAsync().Result;
+            if (message.Equals("\"SUCCESS\""))
+                return Redirect("Details");
+            ModelState.AddModelError("oldPassword", "Wrong password");
+            return View(password);
+        }
+
+        public ActionResult Addresses()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "customer/users/" + user.id + "/addresses";
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            ViewBag.addresses = response.Content.ReadAsAsync<IEnumerable<Address>>().Result;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Addresses(Address address)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "customer/addresses/" + user.id;
+            HttpResponseMessage postResponse = httpClient.PostAsJsonAsync<Address>(url, address).Result;
+            postResponse.EnsureSuccessStatusCode();
+            url = "customer/users/" + user.id + "/addresses";
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            ViewBag.addresses = response.Content.ReadAsAsync<IEnumerable<Address>>().Result;
+            return View();
+        }
+
+        public ActionResult EditAddress(long id)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return RedirectToAction("Login", "User");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "customer/addresses/" + id;
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            return View(response.Content.ReadAsAsync<Address>().Result);
+        }
+
+        [HttpPost]
+        public ActionResult EditAddress(Address address)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return RedirectToAction("Login", "User");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            HttpResponseMessage response = httpClient.PostAsJsonAsync<Address>("customer/addresses/edit", address).Result;
+            if (response.IsSuccessStatusCode)
+                return RedirectToAction("Addresses", "User");
+            return View(address);
+        }
+
+        public ActionResult DeleteAddress(long id)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "customer/addresses/" + id;
+            httpClient.DeleteAsync(url);
+            return RedirectToAction("Addresses", "User");
+        }
+
+        public ActionResult ManageUsers()
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return Redirect("Login");
+            if (user.type != UserType.ADMIN)
+                return RedirectToAction("Index", "Home");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            HttpResponseMessage response = httpClient.GetAsync("customer/users").Result;
+            response.EnsureSuccessStatusCode();
+            return View(response.Content.ReadAsAsync<IEnumerable<User>>().Result);
+        }
+
+        public ActionResult Type(long id, UserType type)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return RedirectToAction("Login", "User");
+            if (user.type != UserType.ADMIN)
+                return RedirectToAction("Index", "Home");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "admin/users/" + id + "/" + type;
+            HttpResponseMessage response = httpClient.PutAsync(url, null).Result;
+            response.EnsureSuccessStatusCode();
+            if (user.id == id)
+            {
+                user.type = type;
+                Session["user"] = user;
+            }
+            return RedirectToAction("ManageUsers", "User");
+        }
+
+        public ActionResult Delete(long id)
+        {
+            User user = (User)Session["user"];
+            if (user == null)
+                return RedirectToAction("Login", "User");
+            if (user.type != UserType.ADMIN)
+                return RedirectToAction("Index", "Home");
+            HttpClient httpClient = HttpClientBuilder.get(Session["api-cookie"]);
+            string url = "admin/users/" + id;
+            HttpResponseMessage response = httpClient.DeleteAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            if (user.id == id)
+                return RedirectToAction("Logout", "User");
+            return RedirectToAction("ManageUsers", "User");
         }
     }
 }
--- ConsommiTounsi/Models/User/User.cs
+++ ConsommiTounsi/Models/User/User.cs
@@ -8,22 +8,31 @@ namespace ConsommiTounsi.Models.User
 {
     public enum UserType
     {
+        [Display(Name = "Customer")]
         CUSTOMER,
+        [Display(Name = "Admin")]
         ADMIN,
+        [Display(Name = "Deliverer")]
         DELIVERER
     }
 
     public enum Gender
     {
+        [Display(Name = "Male")]
         MALE,
+        [Display(Name = "Female")]
         FEMALE,
+        [Display(Name = "Other")]
         OTHER
     }
 
     public enum CivilState
     {
+        [Display(Name = "Married")]
         MARRIED,
+        [Display(Name = "Divorced")]
         DIVORCED,
+        [Display(Name = "Single")]
         SINGLE
     }
 
@@ -45,41 +54,53 @@ namespace ConsommiTounsi.Models.User
 
     public class User
     {
-        public int id { get; set; }
+        public long id { get; set; }
+
         [Display(Name ="First Name")]
         [Required(ErrorMessage = "This field is required")]
         [StringLength(21, ErrorMessage = "It must be between 2 and 21 characters", MinimumLength = 2)]
         public string firstName { get; set; }
+
         [Required(ErrorMessage = "This field is required")]
         [Display(Name = "Last Name")]
         [StringLength(21, ErrorMessage = "It must be between 2 and 21 characters", MinimumLength = 2)]
         public string lastName { get; set; }
+
         [Required(ErrorMessage = "This field is required")]
         [StringLength(21, ErrorMessage = "It must be between 2 and 21 characters", MinimumLength = 2)]
         public string username { get; set; }
+
         [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address")]
         [Required(ErrorMessage = "This field is required")]
         public string email { get; set; }
+
         [Required(ErrorMessage = "This field is required")]
         [DataType(DataType.Password)]
         [StringLength(50, ErrorMessage = "It must be between 5 and 50 characters", MinimumLength = 5)]
         public string password { get; set; }
+
         [Required(ErrorMessage = "This field is required")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression("[0-9]{8}", ErrorMessage = "It must contain 8 numbers")]
         [Display(Name = "Phone Number")]
         public string phoneNumber { get; set; }
+
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "This field is required")]
         [Display(Name = "Birth Date")]
         public DateTime birthDate { get; set; }
+
         [Required(ErrorMessage = "Please select an option")]
         public Gender gender { get; set; }
+
         [Display(Name = "Civil State")]
         [Required(ErrorMessage = "Please select an option")]
         public CivilState civilState { get; set; }
+
         [Required(ErrorMessage = "Please select an option")]
         public Occupation occupation { get; set; }
+
         public UserType type { get; set; }
     }
 }
