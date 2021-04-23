8cb70c2fd4ad5216c1ba3ce5752e930653558831
khalil211 <khalilmajoul999@gmail.com>
2021-04-23 06:34:22 +0100

    Forum: topics + posts + rating + likes / dislikes

---
+++ ConsommiTounsi/Models/Forum/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConsommiTounsi.Models.Forum
+{
+    public class Post
+    {
+        public long id { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(255, ErrorMessage = "You cannot exceed 255 characters")]
+        public string content { get; set; }
+        public string medias { get; set; }
+        public DateTime date { get; set; }
+        public User.User user { get; set; }
+        public List<PostLike> likes { get; set; }
+    }
+}
