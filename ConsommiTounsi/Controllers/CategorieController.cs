83e397cfce95d62b57a6b71b81ed5b18a0b9cad2
eya <eyacharni@esprit.tn>
2021-04-24 18:29:19 +0200

    view

---
+++ ConsommiTounsi/Controllers/CategorieController.cs
@@ -0,0 +1,39 @@
+﻿using ConsommiTounsi.Models.Products;
+using ConsommiTounsi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsommiTounsi.Controllers
+{
+    public class CategorieController : Controller
+    {
+        // GET: Categorie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            HttpClient httpClient = HttpClientBuilder.Get();
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = httpClient.PostAsJsonAsync<Category>("categories", category).Result;
+                response.EnsureSuccessStatusCode();
+
+            }
+            return View(category);
+        }
+    }
+}
